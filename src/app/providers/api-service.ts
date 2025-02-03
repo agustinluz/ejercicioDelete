06665aa00803864d11da0ffd16cb0cae2837649a
agustinluz <agustinluzmartinll@gmail.com>
2025-02-03 11:03:22 +0100

    insertar SIN BORRAR

--- src/app/providers/api-service.ts
+++ src/app/providers/api-service.ts
@@ -106,4 +106,20 @@ buscarAlumnosPorNombreApellido(nombre: string, apellido: string, ciudad: string)
         });
     });
   }
+  insertarAlumno(alumno: Alumno): Promise<Boolean> {
+
+    return new Promise<Boolean>((resolve, reject) => {
+       this.http.post(this.URL + "/alumnos", alumno).toPromise().then(
+         (data: any) => {
+           console.log(data);
+           resolve(true);
+         })
+         .catch((error: Error) => {
+           console.log(error.message);
+           reject(error.message);
+         });
+    });
+      }
+
+      
 }
